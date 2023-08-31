1d4c0d5749a1049de8116fe0893851eea14e43fb
Oscar <oscarcheruiyot3@gmail.com>
2023-08-31 20:02:30 +0300

    restructuring

--- src/app/components/Feed.jsx
+++ src/app/components/Feed.jsx
@@ -1,21 +1,54 @@
 "use client"
 
 import React from 'react';
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 import Videos from './Videos';
+
+
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
-const Feed = () => {
 
-    return (
-        <div>
-    
-        <div>
-            <Videos/>
+const Feeds = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('new');
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => setVideos(data.items))
+
+  }, [selectedCategory]);
+
+  return (
+    <div >
+      <div className='bg-slate-900 p-3 text-white'>
+        <Navbar />
+      </div>
+      <div className='grid bg-black'>
+        <div className=' row-start-1 col-span-1 h-screen border-r-2 border-slate-800'>
+          <Sidebar
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+        </div>
+        <div className='row-start-1 col-span-10 h-screen text-white'>
+          <div className='m-3 flex text-xl head-text'>
+            <p>{selectedCategory}</p>
+            <span className='text-red-400 mx-2'> <h4> Videos  </h4></span>
+ 
             
+          </div>
+          <Videos videos={videos}/>
+
+
         </div>
-        </div>
-    )
+      </div>
+    </div>
+
+
+  )
 }
 
-export default Feed
+export default Feeds
--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,17 @@
-"use client"
 
 import React from 'react';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
-import Videos from './components/Videos';
-import { fetchFromAPI } from './utils/fetchFromAPI';
+
+
 
 
 const HomePage = () => {
 
-  const [selectedCategory, setSelectedCategory] = useState('new');
-  const [videos, setVideos] = useState([])
-
-  useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => setVideos(data.items))
-
-  }, [selectedCategory]);
-
   return (
     <div >
-      <div className='bg-slate-900 p-3 text-white'>
-        <Navbar />
-      </div>
-      <div className='grid bg-black'>
-        <div className=' row-start-1 col-span-1 h-screen border-r-2 border-slate-800'>
-          <Sidebar
-            selectedCategory={selectedCategory}
-            setSelectedCategory={setSelectedCategory}
-          />
-        </div>
-        <div className='row-start-1 col-span-10 h-screen text-white'>
-          <div className='m-3 flex text-xl head-text'>
-            <p>{selectedCategory}</p>
-            <span className='text-red-400 mx-2'> <h4> Videos  </h4></span>
-            <Feed />
-            
-          </div>
-          <Videos videos={videos}/>
-
-
-        </div>
-      </div>
+      
+       <Feed/>
     </div>
 
 
